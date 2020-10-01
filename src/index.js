2bb068a3feeda799982c113d31d57890e1c1da63
RomanBaraban <Romanbash@gmail.com>
2020-10-01 09:47:12 +0300

    fear: add 3 functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    let minRes = 0;
+    if (array === undefined || array.length === 0) return minRes;
+    for (let i = 0; i < array.length; i++){
+      if ( array[i] < minRes){
+          minRes = array[i];
+      }
+    }
+    return minRes
 }
 
 exports.max = function max (array) {
-  return 0;
+    let maxRes = 0;
+    if (array === undefined || array.length === 0) return maxRes;
+    for (let i = 0; i < array.length; i++){
+        if ( array[i] > maxRes){
+            maxRes = array[i];
+        }
+    }
+    return maxRes;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let avgRes = 0;
+    if (array === undefined || array.length === 0) return avgRes;
+    for (let i = 0; i < array.length; i++){
+            avgRes += array[i];
+    }
+    return avgRes/array.length;
 }
